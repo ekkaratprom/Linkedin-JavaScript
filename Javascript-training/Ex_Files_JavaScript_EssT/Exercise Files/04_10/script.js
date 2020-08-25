f50a8b1e1dad2e03f0c54bd00630ec4755aebbe7
EKKARATPRO <ekkarat.promrit@allianz.com>
2020-08-25 10:52:41 +0700

    [Edited]: Objects Constructor

--- Javascript-training/Ex_Files_JavaScript_EssT/Exercise Files/04_10/script.js
+++ Javascript-training/Ex_Files_JavaScript_EssT/Exercise Files/04_10/script.js
@@ -1,14 +1,34 @@
-var course = new Object();
-
-var course = {
-    title: "JavaScript Essential Training",
-    instructor: "Morten Rand-Hendriksen",
-    level: 1,
-    published: true,
-    views: 0,
-    updateViews: function() {
-        return ++course.views;
-    }
+// constructor
+function Course(title, instructor, level, published, views) {
+  this.title = title;
+  this.instructor = instructor;
+  this.level = level;
+  this.published = published;
+  this.views = views;
+  this.updateViews = function () {
+    return ++this.views;
+  };
 }
 
-console.log(course);
+var courses = [
+  new Course("JavaScript Essential Training ", "Bugbigz", 1, true, 0),
+  new Course("ES6", "Eve lee", 1, true, 12345),
+];
+
+var course01 = new Course(
+  "JavaScript Essential Training ",
+  "Bugbigz",
+  1,
+  true,
+  0
+);
+var course02 = new Course("ES6", "Eve lee", 1, true, 12345);
+
+console.log(course01);
+console.log(course02);
+
+console.log(courses);
+
+console.log(courses[1].instructor);
+
+console.log(courses[1].updateViews());
