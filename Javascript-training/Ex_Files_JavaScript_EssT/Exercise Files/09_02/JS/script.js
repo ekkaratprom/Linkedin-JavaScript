c8536d216a22187bd74b3daa80a7fe05fb6eef48
EKKARATPRO <ekkarat.promrit@allianz.com>
2020-08-25 17:37:56 +0700

    [Edited]: loop through arrays

--- Javascript-training/Ex_Files_JavaScript_EssT/Exercise Files/09_02/JS/script.js
+++ Javascript-training/Ex_Files_JavaScript_EssT/Exercise Files/09_02/JS/script.js
@@ -1,1 +1,10 @@
+var exLinks = document.querySelectorAll('a[href^="http"]')
+console.log(exLinks);
 
+for (var i = 0; i < exLinks.length; i++){
+    // console.log(exLinks[i]);
+    if (!exLinks[i].hasAttribute("target")){
+        exLinks[i].setAttribute("target","_blank");
+
+    }
+}
