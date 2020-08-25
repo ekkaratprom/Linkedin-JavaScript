5425a601829814bb03e4cea7e2da6bd163d5fa0a
EKKARATPRO <ekkarat.promrit@allianz.com>
2020-08-25 13:18:11 +0700

    [Edited]: DOM Elements

--- Javascript-training/Ex_Files_JavaScript_EssT/Exercise Files/05_06/JS/script.js
+++ Javascript-training/Ex_Files_JavaScript_EssT/Exercise Files/05_06/JS/script.js
@@ -1,1 +1,19 @@
+const FEATURED = document.querySelector(".featured-image");
+const THEIMAGE = FEATURED.querySelector("img");
 
+var altText = THEIMAGE.getAttribute("alt");
+
+var captionElement = document.createElement("figcaption");
+
+var captionText = document.createTextNode(altText);
+
+//append
+// captionElement.appendChild(captionText);
+
+// FEATURED.appendChild(captionText);
+// THEIMAGE.setAttribute("alt", "");
+
+captionElement.append(altText);
+FEATURED.append(captionElement);
+
+THEIMAGE.setAttribute("alt", "");
--- Javascript-training/Ex_Files_JavaScript_EssT/Exercise Files/05_07/JS/script.js
+++ Javascript-training/Ex_Files_JavaScript_EssT/Exercise Files/05_07/JS/script.js
@@ -1,1 +1,12 @@
+const FEATURED = document.querySelector(".featured-image");
+const THEIMAGE = FEATURED.querySelector("img");
 
+var altText = THEIMAGE.getAttribute("alt");
+
+var captionElement = document.createElement("figcaption");
+
+var captionText = document.createTextNode(altText);
+
+captionElement.appendChild(captionText);
+
+console.log(captionElement);
