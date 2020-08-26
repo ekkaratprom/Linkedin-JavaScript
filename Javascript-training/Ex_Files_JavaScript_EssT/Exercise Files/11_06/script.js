d84ebd388a8cb3842bee18edb39854822af07244
EKKARATPRO <ekkarat.promrit@allianz.com>
2020-08-26 13:25:50 +0700

    [Edited]: Automate script eslint

--- Javascript-training/Ex_Files_JavaScript_EssT/Exercise Files/11_06/script.js
+++ Javascript-training/Ex_Files_JavaScript_EssT/Exercise Files/11_06/script.js
@@ -1,10 +1,10 @@
 const testWrapper = document.querySelector(".test-wrapper");
-const testArea = document.querySelector("#test-area")
+const testArea = document.querySelector("#test-area");
 const originText = document.querySelector("#origin-text p").innerHTML;
 const resetButton = document.querySelector("#reset");
 const theTimer = document.querySelector(".timer");
 
-timer = [0,0,0,0]
+var timer = [0, 0, 0, 0];
 var interval;
 var timerRunning = false;
 
@@ -18,20 +18,25 @@ function leadingZero(time) {
 
 // Run a standard minute/second/hundredths timer:
 function runTimer() {
-    let currentTime = leadingZero(timer[0]) + ":" + leadingZero(timer[1]) + ":" + leadingZero(timer[2]);
+    let currentTime =
+    leadingZero(timer[0]) +
+    ":" +
+    leadingZero(timer[1]) +
+    ":" +
+    leadingZero(timer[2]);
     theTimer.innerHTML = currentTime;
     timer[3]++;
 
-    timer[0] = Math.floor((timer[3]/100)/60);
-    timer[1] = Math.floor((timer[3]/100) - (timer[0] * 60));
-    timer[2] = Math.floor(timer[3] - (timer[1] * 100) - (timer[0] * 6000));
+    timer[0] = Math.floor(timer[3] / 100 / 60);
+    timer[1] = Math.floor(timer[3] / 100 - timer[0] * 60);
+    timer[2] = Math.floor(timer[3] - timer[1] * 100 - timer[0] * 6000);
 }
 
 // Match the text entered with the provided text on the page:
 function spellCheck() {
     let textEntered = testArea.value;
-    let originTextMatch = originText.substring(0,textEntered.length);
-    let textMatch;
+    let originTextMatch = originText.substring(0, textEntered.length);
+
 
     if (textEntered == originText) {
         clearInterval(interval);
@@ -43,7 +48,6 @@ function spellCheck() {
             testWrapper.style.borderColor = "#E95D0F";
         }
     }
-
 }
 
 // Start the timer:
@@ -60,11 +64,11 @@ function start() {
 function reset() {
     clearInterval(interval);
     interval = null;
-    timer = [0,0,0,0];
+    timer = [0, 0, 0, 0];
     timerRunning = false;
 
     testArea.value = "";
-    theTimer.innerHTML = '00:00:00';
+    theTimer.innerHTML = "00:00:00";
     testWrapper.style.borderColor = "grey";
 }
 
